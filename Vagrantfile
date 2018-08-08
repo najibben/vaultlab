c9853c81f1879908dede39bbedda327f35461b9b
Najib Ben Haddou Perez <najib@Najibs-MacBook-Pro.local>
2018-08-08 16:56:18 +0200

    add initial install mysql script

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,4 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "cbednarski/ubuntu-1604"
+  config.vm.provision :shell, :path => "scripts/install_mysql.sh"  
 end
---
+++ scripts/install_mysql.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# check if mysql is install
+# if mysql is not installed install mysql
+
+# check if user is created 
+# if user is not created create user
+
+# test user and password
+
+exit 1
+
+
