714cf9eb0360e821837bcd8387017af62c791e22
Najib Ben Haddou Perez <najib@Najibs-MacBook-Pro.local>
2018-08-09 13:23:09 +0200

    create api to get data from dbname

--- app.py
+++ app.py
@@ -8,18 +8,25 @@ import MySQLdb
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="najib",         # your username
                      passwd="test123",  # your password
-                     db="testdb")        # name of the data base
+                     db="dbname")        # name of the data base
 
 # you must create a Cursor object. It will let
 #  you execute all the queries you need
 cur = db.cursor()
 
 # run a simple select
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
 
-# print all the first cell of all the rows
+cur.execute("SELECT * from Persons")
+
+# print all the cell of  all the rows
 for row in cur.fetchall():
     print row[0]
+    print  row[1]
+ 
+
+
+#for firstname, lastname in cur.fetchall() :
+#        print firstname, lastname
+
 
 db.close()
-
