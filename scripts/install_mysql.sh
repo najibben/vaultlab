deee8bc7d1830d573cfb0ca3acfe418a48b58edc
Najib Ben Haddou Perez <najib@Najibs-MacBook-Pro.local>
2018-08-09 09:34:18 +0200

    check if mysql is present

--- scripts/install_mysql.sh
+++ scripts/install_mysql.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
 # check if mysql is install
+type mysql >/dev/null 2>&1 && echo "MySQL present." || echo "MySQL not present."
+
+: '
+> is for redirect
+
+/dev/null is a black hole where any data sent, will be discarded
+
+2 is the file descriptor for Standard Error
+
+> is for redirect
+
+& is the symbol for file descriptor (without it, the following 1 would be considered a filename)
+
+1 is the file descriptor for Standard Out
+
+Therefore >/dev/null 2>&1 is redirect the output of your program to /dev/null. Include both the  Standard Error and Standard Out.
+
+'
+
 # if mysql is not installed install mysql
+
 
 # check if user is created 
 # if user is not created create user
 
 # test user and password
-
-exit 1
+ exit 0
 
 
