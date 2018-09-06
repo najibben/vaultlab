bfc176c00539e50c6f178e343d5981b3a1f625ba
Najib Ben Haddou Perez <najib@Najibs-MacBook-Pro.local>
2018-09-06 19:09:25 +0200

    use_case_1

--- etc/vault/consul.hcl
+++ etc/vault/consul.hcl
@@ -1,12 +1,13 @@
 vault {
-  address = "https://vault.service.consul:8200"
-  token   = VAULT_TOKEN
-  renew   = true
-
-  ssl {
-    enabled = false
-    verify  = false
-    cert    = "/path/to/client/cert.pem"
-    ca_cert = "/path/to/ca/cert.pem"
-  }
+  address = "http://192.168.2.10:8200"
+  token   = "6212da45-329e-814c-2200-faa8973a8b33"
+}
+ ssl {
+        enabled = true
+        verify = false
+    }
+secret {
+  no_prefix = true
+  path      = "kv/password"
+ format = "secret_{{ key }}"
 }
--- scripts/bash_vault.sh
+++ scripts/bash_vault.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
+which jq &>/dev/null || {
+echo "jq not present."
+apt install -y jq
 
-USER=$(vault kv get secret/prometheus | grep user | awk '{print $2}')
-PASSWD=$(vault kv get secret/prometheus | grep password | awk '{print $2}')
-curl -so envconsul.tgz https://releases.hashicorp.com/envconsul/0.7.3/envconsul_0.7.3_linux_amd64.tgz
-tar -xvzf envconsul.tgz
-mv envconsul /usr/local/bin/envconsul
-chmod +x /usr/local/bin/envconsul
-
-consul agent -dev
-consul kv put my-app/max_conns 5
-
- echo envconsul -prefix my-app env
+#this is for vault secret engine direct reading
+#USER=$(vault kv get secret/prometheus | grep user | awk '{print $2}')
+USER=$(curl -s --header "X-Vault-Token:$VAULT_TOKEN" --request GET --data @payload.json --insecure http://192.168.2.10:8200/v1/kv/user |  jq -r '.data' | awk '{print$2}' | perl -ne 'print if /\S/'  | sed -e 's/^"//' -e 's/"$//')
+#this is for vault secret engine direct reading
+#PASSWD=$(vault kv get secret/prometheus | grep password | awk '{print $2}')
+PASSWD=$(curl -s --header "X-Vault-Token:$VAULT_TOKEN" --request GET --data @payload.json --insecure http://192.168.2.10:8200/v1/kv/password |  jq -r '.data' | awk '{print$2}' | perl -ne 'print if /\S/'  | sed -e 's/^"//' -e 's/"$//') 
 
 # create db
 echo -e "Setting up our MySQL user and db"
@@ -25,3 +23,4 @@ mysql -u "$USER" -p"$PASSWD" -e "SHOW DATABASES"
 
 echo -n '{"value":"najib"}' | vault kv put secret/user -
 echo -n '{"value":"test123"}' | vault kv put secret/password -
+
